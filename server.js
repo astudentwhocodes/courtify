c84e76c1c2b451bc0fea2d15a09938d99ca311c6
Zese07 <128886474+Zese07@users.noreply.github.com>
2024-05-11 13:14:23 +0800

    U

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql');
 const path = require('path'); 
-const fs = require('fs'); // Import the 'fs' module
 
 // Import user routes
 const userRoutes = require('./config/user');
@@ -14,8 +13,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the 'src' directory
+app.use(express.static(path.join(__dirname, 'src')));
 
 // Create MySQL connection pool
 const pool = mysql.createPool({
@@ -33,16 +32,9 @@ app.use((req, res, next) => {
     next();
 });
 
-// Handle render route
+// Serve index.html on GET request to /
 app.get('/', (req, res) => {
-    const indexPath = path.join(__dirname, '/src/index.html');
-    fs.access(indexPath, fs.constants.F_OK, (err) => {
-        if (err) {
-            res.send('Welcome to Courtify'); // If index.html doesn't exist
-        } else {
-            res.sendFile(indexPath); // If index.html exists, send it
-        }
-    });
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 // Use user routes
