d2aa01e8b8c74cca752243699d73abddaca8997a
Zese07 <128886474+Zese07@users.noreply.github.com>
2024-05-12 14:05:04 +0800

    U

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const cors = require('cors');
 const mysql = require('mysql');
 const path = require('path'); 
 
-// Import user routes
 const userRoutes = require('./src/config/user.js');
 const userInfoRoutes = require('./src/config/user_info.js');
 const courtInfoRoutes = require('./src/config/court_info.js');
 const courtRoutes = require('./src/config/court.js');
 const dashboardInfoRoutes = require('./src/config/dashboard_info.js');
 const bookCourtRoutes = require('./src/config/book.js');
-const imageRoutes = require('./src/config/image.js'); // Import image routes
+const imageRoutes = require('./src/config/image.js');
 
 const app = express();
 
@@ -21,31 +20,26 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Serve static files from the 'src' directory
 app.use(express.static(path.join(__dirname, 'src')));
 
-// Create MySQL connection pool
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'sql6.freemysqlhosting.net',
     user: 'sql6705169',
     password: 'lafXyFPRFm',
-    database: 'courtify',
-    port: 3306 // Port number
+    database: 'sql6705169',
+    port: 3306 
 });
 
-// Pass the MySQL pool to routes
 app.use((req, res, next) => {
     req.pool = pool;
     next();
 });
 
-// Serve index.html on GET request to /
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
-// Use user routes
 app.use('/api', userRoutes); // Assuming user routes start with '/api'
 app.use('/userinfo', userInfoRoutes); // Assuming user routes start with '/api'
 app.use('/courtinfo', courtInfoRoutes); // Assuming user routes start with '/api'
